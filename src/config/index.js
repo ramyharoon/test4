e828f3685a7749839e61952cef8efd6ac3b9ffe3
joe <joe@krolm.com>
2020-03-09 14:36:30 +0800

    Update Backend URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,3 @@
 export const LOCAL_TEST = true
 export const SHOW_LOG = true
-export const BACK_END_URL = "https://webbill-frontend.herokuapp.com"
+export const BACK_END_URL = "https://webbill-backend.herokuapp.com"
